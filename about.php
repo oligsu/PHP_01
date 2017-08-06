f846131aa737a871a33824f88a74167217ce7fd6
User <olig.su@googlemail.com>
2017-08-07 00:13:56 +0500

    + file about.php

---
+++ about.php
@@ -0,0 +1,23 @@
+<?php
+$name='Alexandr';
+echo 'name ';
+echo $name;
+echo '<br>';
+$age = 40;
+echo 'Age ';
+echo $age;
+echo '<br>';
+$email ='olig.sugooglemail.com';
+echo 'email <a href="mailto:';
+echo $email;
+echo '">olig.su@googlemail.com</a>';
+echo '<br>';
+$city='Muravenko';
+echo 'city ';
+echo $city;
+echo '<br>';
+$about='specialist';
+echo 'about ';
+echo $about;
+
+?>
